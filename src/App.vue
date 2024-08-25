48817327f99da888e1fdc6ed8f27b3b8e470b280
ze3ck <bastianleyton0.8@gmail.com>
2024-08-25 03:37:58 -0400

    Under Construction
    OptiFlow -> Under Construction Mode

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <UnderConstruction />
+  </div>
+</template>
+
+<script>
+import UnderConstruction from './components/underConstruction.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UnderConstruction
+  }
+}
+</script>
