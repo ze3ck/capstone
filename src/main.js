1e4193be9dcd4445f48118e8940f87d90042e4d1
bleytonm <bastian.leyton@chapapp.cl>
2024-08-29 20:43:59 -0400

    Update evidencias

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './components/underConstruction.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-createApp(App).mount('#app')
