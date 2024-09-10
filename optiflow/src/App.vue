18ab08638ac254af390b26a670feafb5f12acfec
CristianUbilla <cri.ubilla@duocuc.cl>
2024-09-09 23:40:27 -0300

    Login incompleto
    Falta un poco de diseño

---
+++ optiflow/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <LoginForm />
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'; // Asegúrate de que el nombre coincida
+
+export default {
+  name: 'App',
+  components: { LoginForm } // Usa el nombre correcto del componente
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
