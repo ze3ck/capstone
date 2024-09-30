c45ffb76fdce1f60056bd264899e0b71920a21fd
ze3ck <bastianleyton0.8@gmail.com>
2024-09-30 05:22:27 -0300

    act db

--- opti-front/includes/db.php
+++ opti-front/includes/db.php
@@ -1,17 +1,28 @@
 <?php
-// ajustar credenciales para hosting -> .env del backend
-$servername = "localhost";
-$username = "root";
-$password = "hola";
-$dbname = "c2611566_OPTFW_DEV";
+// Incluir la librería Dotenv
+require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+// Cargar el archivo .env
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../includes');
+$dotenv->load();
+
+// Obtener credenciales del archivo .env
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+// Conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Comprobar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-function cerrarConexion($conn) {
+function cerrarConexion($conn)
+{
     $conn->close();
 }
-?>
